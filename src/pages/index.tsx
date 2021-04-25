742c8f822b4c0425cb043e5c7485eef018a9dc37
Walker de Alencar <walkeralencar@gmail.com>
2021-04-25 03:07:25 -0300

    Ajustes no box dos elementos, margin e padding zerados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,8 @@ export default function Home(props) {
 
   return (
     <div>
-    <h1>index</h1>
-    <p>{JSON.stringify(props.episodes)}</p>
+      <h1>index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
     </div>
   )
 }
