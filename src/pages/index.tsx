aa0f588462953c436dafd7f48dfed3aee9d2f395
Walker de Alencar <walkeralencar@gmail.com>
2021-04-25 01:55:15 -0300

    SPA mode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
-import { Header } from "../components/Header";
+// SPA - Single Page Application
+// SSR - Server Side Rendering
+// SSG - Static Site Generator
+
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect (() => {
+    fetch('http://local.test:3333/episodes')
+      .then(response => response.json())
+      .then(data => console.log(data))
+    },[])
   return (
     <h1>index</h1>
   )
