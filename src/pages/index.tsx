e8c014db757fadfea763403671242ae3f6651af3
Walker de Alencar <walkeralencar@gmail.com>
2021-04-25 01:58:10 -0300

    SGG Mode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
-import { Header } from "../components/Header";
+// SSG - Static Site Generator
 
-export default function Home() {
+export default function Home(props) {
+
   return (
+    <div>
     <h1>index</h1>
+    <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://local.test:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 2,
+  }
 }
